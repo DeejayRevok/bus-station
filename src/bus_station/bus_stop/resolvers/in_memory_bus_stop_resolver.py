7548bebf3e03e2133810aa97f1ca785ea9c83bd5
DeejayRevok <seryi_one@hotmail.com>
2023-07-07 01:32:05 +0200

    fix: fix bus resolvers typing

--- src/bus_station/bus_stop/resolvers/in_memory_bus_stop_resolver.py
+++ src/bus_station/bus_stop/resolvers/in_memory_bus_stop_resolver.py
@@ -1,19 +1,17 @@
-from typing import Dict, Optional, TypeVar
+from typing import Dict, Optional
 
 from bus_station.bus_stop.bus_stop import BusStop
 from bus_station.bus_stop.resolvers.bus_stop_resolver import BusStopResolver
 from bus_station.shared_terminal.fqn import resolve_fqn
 
-S = TypeVar("S", bound=BusStop)
 
-
-class InMemoryBusStopResolver(BusStopResolver[S]):
+class InMemoryBusStopResolver(BusStopResolver):
     def __init__(self):
-        self.__bus_stops: Dict[str, S] = {}
+        self.__bus_stops: Dict[str, BusStop] = {}
 
-    def add_bus_stop(self, bus_stop: S) -> None:
+    def add_bus_stop(self, bus_stop: BusStop) -> None:
         fqn = resolve_fqn(bus_stop)
         self.__bus_stops[fqn] = bus_stop
 
-    def resolve(self, bus_stop_id: str) -> Optional[S]:
+    def resolve(self, bus_stop_id: str) -> Optional[BusStop]:
         return self.__bus_stops.get(bus_stop_id)
--- src/bus_station/bus_stop/resolvers/pypendency_bus_stop_resolver.py
+++ src/bus_station/bus_stop/resolvers/pypendency_bus_stop_resolver.py
@@ -1,18 +1,16 @@
-from typing import Optional, TypeVar
+from typing import Optional
 
 from pypendency.container import Container
 
 from bus_station.bus_stop.bus_stop import BusStop
 from bus_station.bus_stop.resolvers.bus_stop_resolver import BusStopResolver
 
-S = TypeVar("S", bound=BusStop)
 
-
-class PypendencyBusStopResolver(BusStopResolver[S]):
+class PypendencyBusStopResolver(BusStopResolver):
     def __init__(self, pypendency_container: Container):
         self.__pypendency_container = pypendency_container
 
-    def resolve(self, bus_stop_id: str) -> Optional[S]:
+    def resolve(self, bus_stop_id: str) -> Optional[BusStop]:
         resolved_instance = self.__pypendency_container.get(bus_stop_id)
         if resolved_instance is None:
             return None
