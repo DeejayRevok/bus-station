c67db49bc15956dd152909ff78f90549f530899a
DeejayRevok <seryi_one@hotmail.com>
2023-07-13 01:48:39 +0200

    refactor: simplify bus stop address registries code removing the need of passenger resolver

--- src/bus_station/query_terminal/query_handler_registry.py
+++ src/bus_station/query_terminal/query_handler_registry.py
@@ -1,26 +1,12 @@
-from functools import partial
 from typing import Optional
 
 from bus_station.bus_stop.registration.bus_stop_registry import BusStopRegistry
-from bus_station.bus_stop.resolvers.bus_stop_resolver import BusStopResolver
 from bus_station.passengers.passenger_registry import passenger_bus_stop_registry
-from bus_station.passengers.passenger_resolvers import resolve_passenger_class_from_bus_stop
 from bus_station.query_terminal.handler_not_found_for_query import HandlerNotFoundForQuery
 from bus_station.query_terminal.query_handler import QueryHandler
 
 
 class QueryHandlerRegistry(BusStopRegistry[QueryHandler]):
-    def __init__(
-        self,
-        bus_stop_resolver: BusStopResolver,
-    ):
-        bus_stop_passenger_resolver = partial(
-            resolve_passenger_class_from_bus_stop,
-            bus_stop_handle_function_name="handle",
-            passenger_type_name="query",
-        )
-        super().__init__(bus_stop_resolver, bus_stop_passenger_resolver)
-
     def get_handler_from_query(self, query_name: str) -> Optional[QueryHandler]:
         query_handler_ids = passenger_bus_stop_registry.get_bus_stops_for_passenger(query_name)
         if len(query_handler_ids) == 0:
